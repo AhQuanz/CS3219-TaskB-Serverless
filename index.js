1e8eecd16aaa5d38f8805ab604b2540f563255fa
Li Quan <linliquan1996@hotmail.com>
2022-09-18 20:36:17 +0800

    include nusMod API

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-exports.serverlessFunction = (req, res) => {
-  let message = req.query.message || "Local Cloud Function";
-  res.status(200).send(message);
+const axios = require("axios");
+
+exports.serverlessFunction = async (req, res) => {
+  try {
+    const { year, modulePrefix } = req.query;
+    const nextYear = parseFloat(year) + 1;
+    const resp = await axios.get(
+      `https://api.nusmods.com/v2/${year}-${nextYear}/moduleList.json`
+    );
+    let modules = [];
+    resp.data.map((module) => {
+      if (module.moduleCode.includes(modulePrefix)) {
+        modules.push(module.moduleCode);
+      }
+    });
+    modules = modules.slice(0, 10);
+    if (modules.length) {
+      return res.status(200).send({ modules: modules });
+    } else {
+      return res.status(201).send("No modules found");
+    }
+  } catch (err) {
+    console.log(err.message);
+    return res.status(404).send("Bad Request");
+  }
 };
